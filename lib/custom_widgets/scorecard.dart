adf6a18ab6505d45de4fd1b34380acbb90e5178f
muhammadjunaidkhalid <mjk22071998@gmail.com>
2022-04-16 23:20:59 +0500

    Scorecard decorated

--- lib/custom_widgets/scorecard.dart
+++ lib/custom_widgets/scorecard.dart
@@ -1,30 +1,35 @@
 import 'package:flutter/material.dart';
 
 Widget scorecard(String title, String info) {
-  return Container(
-    margin: const EdgeInsets.all(26.0),
-    padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 26.0),
-    color: Colors.white,
-    child: Column(
-      children: [
-        Text(
-          title,
-          style: const TextStyle(
-            fontSize: 22.0,
-            fontWeight: FontWeight.bold,
+  return Expanded(
+    child: Container(
+      margin: const EdgeInsets.all(26.0),
+      padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 26.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(6.0),
+        color: Colors.white,
+      ),
+      child: Column(
+        children: [
+          Text(
+            title,
+            style: const TextStyle(
+              fontSize: 22.0,
+              fontWeight: FontWeight.bold,
+            ),
           ),
-        ),
-        const SizedBox(
-          height: 6.0,
-        ),
-        Text(
-          info,
-          style: const TextStyle(
-            fontSize: 34.0,
-            fontWeight: FontWeight.bold,
+          const SizedBox(
+            height: 6.0,
           ),
-        )
-      ],
+          Text(
+            info,
+            style: const TextStyle(
+              fontSize: 34.0,
+              fontWeight: FontWeight.bold,
+            ),
+          )
+        ],
+      ),
     ),
   );
 }
